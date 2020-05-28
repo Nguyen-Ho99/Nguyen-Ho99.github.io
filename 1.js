41c9fa72f564d3ef2b453939304ebbef1b8f3abc
Ho Cong Nguyen <nguyenk39a9@gmail.com>
2020-05-28 21:52:30 +0700

    demoweb

---
+++ 1.js
@@ -0,0 +1,44 @@
+﻿document.addEventListener("DOMContentLoaded",function(){
+var menu=document.querySelectorAll('nav');
+var ac=document.getElementsByClassName('nav.navbar.navbar-light.bg-faded.menutop')
+var sticky=document.getElementsByClassName('nutlen');
+var scroll=document.querySelector('.scrollTop');
+var menu=menu[0];
+var trangthai="duoi200";
+
+window.addEventListener("scroll",function(){
+   var x = pageYOffset;
+  
+   
+   if(x > 100){
+            if(trangthai == "duoi200")
+            {
+                trangthai="tren200";
+                menu.classList.remove('menutop');
+                menu.classList.add('menutora');
+           
+            }
+        }
+        else{
+            if(trangthai=="tren200"){
+            menu.classList.remove('menutora');
+              menu.classList.add('menutop');
+            trangthai="duoi200";}
+        }
+      if (x>400) {
+         sticky[0].classList.add('back-to-top');
+
+      }
+      else if(x<2000){
+          sticky[0].classList.remove('back-to-top');
+      }
+})
+function scrollToTop(){console.log("k kich dc");
+   window.scrollTo({
+      top:0,
+      behavior:'smooth'
+
+   })
+}
+
+},false)
